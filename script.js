d83fc965ee8143623d5ddac3e4fd54841c8de62e
Hridoy-Modak <hridoychandramodak@gmail.com>
2024-06-05 14:48:49 +0600

    upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn=document.querySelector(".btn");
+let heading= document.querySelector('.heading');
+
+btn.addEventListener('click', function(){
+       heading.innerHTML="I Love You";
+    })
+
+
